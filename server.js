accedc8b50011124d4c7c023d63512d319b3d5f3
Révész Márton <revesz.marton@pomscloud.ie>
2025-07-23 18:56:37 +0200

    refactor: move massages array to frontend

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-import express from 'express';
-import { getAiAnswer } from './services/ai.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import { getAiAnswer } from "./services/ai.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'https://revmarci.github.io'
-}));
+app.use(
+    cors({
+        origin: "https://revmarci.github.io",
+    })
+);
 
 app.use(express.json());
 
@@ -21,11 +23,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // API endpoint
-app.post('/ask', async (req, res) => {
-    const { question } = req.body;
+app.post("/ask", async (req, res) => {
+    const messages = req.body;
 
     try {
-        const answer = await getAiAnswer(question);
+        const answer = await getAiAnswer(messages);
         res.json({ answer });
     } catch (error) {
         res.status(500).json({ error: error.message });
