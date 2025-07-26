83a828cd4fdacb213e572255d4708bfad36a1dfd
Révész Márton <revesz.marton@pomscloud.ie>
2025-07-26 23:23:29 +0200

    feat: add wake up endpoint

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-import express from "express";
-import { getAiAnswer } from "./services/ai.js";
-import path from "path";
-import { fileURLToPath } from "url";
-import dotenv from "dotenv";
-import cors from "cors";
+import express from 'express';
+import { getAiAnswer } from './services/ai.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+/*
 app.use(
     cors({
-        origin: "https://revmarci.github.io",
+        origin: 'https://revmarci.github.io',
     })
-);
+);*/
+app.use(cors());
 
 app.use(express.json());
 
@@ -22,8 +23,13 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Wake up
+app.get('/wake', async (req, res) => {
+    res.json({ success: true, message: 'Server is awake.' });
+});
+
 // API endpoint
-app.post("/ask", async (req, res) => {
+app.post('/ask', async (req, res) => {
     const messages = req.body;
 
     try {
